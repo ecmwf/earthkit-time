6f8b7c76634f497f5e893dd68775683b60578655
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2024-07-26 16:27:55 +0000

    Use Read The Docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
+    "sphinx_rtd_theme"
 ]
 
 templates_path = ["_templates"]
@@ -37,7 +38,7 @@ autodoc_member_order = "bysource"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 
